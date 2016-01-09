d5feb219cbab3a5e2219a88fd92066d7ec413f66
Anders Liden <st@liden.cx>
2016-01-09 10:25:04 +0100

    Initial Commit

---
+++ Dice Roller/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Dice Roller
+//
+//  Created by Anders Lidén on 2016-01-09.
+//  Copyright © 2016 Anders Lidén. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Dice Roller/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dice Roller
+//
+//  Created by Anders Lidén on 2016-01-09.
+//  Copyright © 2016 Anders Lidén. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
