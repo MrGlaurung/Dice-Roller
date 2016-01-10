58419b91ed3a91f394275dda924f3363d502b070
Anders Liden <st@liden.cx>
2016-01-10 13:23:25 +0100

    Initial commit. Only IDE code. Nothing done myself.

--- Dice Roller/ViewController.swift
+++ Dice Roller/ViewController.swift
@@ -8,17 +8,20 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
-    override func viewDidLoad() {
+class ViewController: NSViewController
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
+    override var representedObject: AnyObject?
+    {
+        didSet
+        {
+            // Update the view, if already loaded.
         }
     }
 
